6373ba80c02f93a6bac0fd0edbef07c7f05d48c5
Rabindra Dahal <rabindradahal2076@gmail.com>
2025-11-25 19:02:02 +0545

    Added sign in and sign up screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,30 @@
+import { images } from '@/constants';
 import { Slot } from 'expo-router';
 import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
 const _Layout = () => {
   return (
-    <SafeAreaView>
-      <Text>Auth Layout</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView 
+      behavior={Platform.OS === 'android' ? 'height': 'padding'}
+      
+    >
+      <ScrollView 
+        className='bg-white h-full'
+        keyboardShouldPersistTaps="handled"
+      >
+        <View className='w-full h-full' style={{ height: Dimensions.get('screen').height / 2.25}} >
+          <ImageBackground 
+            source={images.loginGraphic} 
+            resizeMode='stretch'
+            className='size-full rounded-b-lg' />
+          <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10' />
+
+        </View>
+        
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const AppWriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: 'com.rd.fastfood',
+    databaseId: '6925836b00309a6c1302',
+    userCollectionId: 'user',
+} 
